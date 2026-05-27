4545ec5e77a5435aec70ab8f6a96ec13b5bb808d
bibirius <bibirius@gmail.com>
2026-05-26 17:17:28 -0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+      <h1 className="text-6xl font-bold mb-4">
+        Bibianarius la mejor
+      </h1>
+
+      <p className="text-xl text-gray-300">
+        Engineer • Researcher • Builder
+      </p>
+    </main>
+  );
+}
